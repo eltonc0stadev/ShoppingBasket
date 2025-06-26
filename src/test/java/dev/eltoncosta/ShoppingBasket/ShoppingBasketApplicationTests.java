cfc6f28e72dcf8b4358a7afc0f255b6b6ce937b4
eltonc0stadev <eltonalcancosta@gmail.com>
2025-06-26 18:45:32 -0300

    Intial commit

---
+++ src/test/java/dev/eltoncosta/ShoppingBasket/ShoppingBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eltoncosta.ShoppingBasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
